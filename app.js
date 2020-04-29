0a7a40d446050d9cbac3467e487f5c40ce538dd3
Prasad Sawant <prasad.sawant20397@gmail.com>
2020-04-30 01:20:05 +0530

    get data from form

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const request  = require("request");
 
 const app = new express();
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
@@ -10,6 +11,13 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
 })
 
+app.post("/", function(req,res){
+    const data = req.body;
+    let firstName = data.firstName;
+    let lastName = data.lastName;
+    let email = data.email;
+    console.log(data);
+})
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
